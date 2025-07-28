84d4cfe18fe3c3f78f92958cd49d080b25d85020
Andrew Taylor <rolyata47@gmail.com>
2025-07-28 13:08:20 -0500

    Updating the code for translating x to a point within [0, pi/2]

--- trig.py
+++ trig.py
@@ -1,17 +1,17 @@
 import math
 
-def translate(x):
-    sign = -1 if x < 0 else 1
+def sin(x):
+    sign = 1 if x > 0 else -1
     x = abs(x) % (2*math.pi)
+    if x > math.pi:
+        sign *= -1
+        x = 2*math.pi - x
     if x > math.pi/2:
         x = math.pi - x
-    return sign * x
-
-def sin(x):
+    x = sign * x
     n = 30
     if n % 2 != 0:
         raise ValueError("n has to be even")
-    x = translate(x)
     lb = 0
     for i in range(0, n):
         if i % 2 == 0:
@@ -27,7 +27,6 @@ def cos(x):
     n = 30
     if n % 2 != 0:
         raise ValueError("n has to be even")
-    x = translate(x)
     lb = 0
     for i in range(0, n):
         if i % 2 == 0:
@@ -43,5 +42,4 @@ print("sin(0) = %f" %sin(0))
 print("sin(1) = %f" %sin(1))
 print("sin(10) = %f" %sin(10))
 print("sin(1.2) = %f" %sin(1.2))
-print("cos(0) = %f" %cos(0))
-print("cos(1) = %f" %cos(1))
+print("sin(-10) = %f" %sin(-10))

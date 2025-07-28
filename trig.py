2fc524f482c6054a4884195360968e7b37d6706d
Andrew Taylor <rolyata47@gmail.com>
2025-07-28 14:30:40 -0500

    Translating x to a point within [0, Pi/2] in the cos(x) function

--- trig.py
+++ trig.py
@@ -8,7 +8,6 @@ def sin(x):
         x = 2*math.pi - x
     if x > math.pi/2:
         x = math.pi - x
-    x = sign * x
     n = 30
     if n % 2 != 0:
         raise ValueError("n has to be even")
@@ -21,9 +20,16 @@ def sin(x):
     ub = lb + x**(2*n+1) / math.factorial(2*n+1)
     if lb != ub:
         raise ValueError("n is too small")
-    return lb
+    return sign * lb
 
 def cos(x):
+    sign = 1
+    x = abs(x) % (2*math.pi)
+    if x > math.pi:
+        x = 2*math.pi - x
+    if x > math.pi / 2:
+        sign = -1
+        x = math.pi - x
     n = 30
     if n % 2 != 0:
         raise ValueError("n has to be even")
@@ -36,7 +42,7 @@ def cos(x):
     ub = lb + x**(2*n) / math.factorial(2*n)
     if lb != ub:
         raise ValueError("n is too small")
-    return lb
+    return sign * lb
 
 print("sin(0) = %f" %sin(0))
 print("sin(1) = %f" %sin(1))
@@ -45,3 +51,11 @@ print("sin(1.2) = %f" %sin(1.2))
 print("sin(-10) = %f" %sin(-10))
 print("sin(-7) = %f" %sin(-7))
 print("sin(-3) = %f" %sin(-3))
+
+print("cos(0) = %f" %cos(0))
+print("cos(1) = %f" %cos(1))
+print("cos(10) = %f" %cos(10))
+print("cos(1.2) = %f" %cos(1.2))
+print("cos(-10) = %f" %cos(-10))
+print("cos(-7) = %f" %cos(-7))
+print("cos(-3) = %f" %cos(-3))

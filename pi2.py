4177715420a9e2e8ce14caa89e95b0a25251dbe9
Andrew Taylor <rolyata47@gmail.com>
2025-07-29 11:30:24 -0500

    Updating pi2.py

--- pi2.py
+++ pi2.py
@@ -1,6 +1,9 @@
+import time
+st = time.time()
+
 lb = 0
 ub = 0
-n = 10000000
+n = 10**9
 
 for i in range(0, n):
     if i % 2 == 0:
@@ -12,9 +15,11 @@ ub = lb + 1/(2*n+1)
 lb *= 4
 ub *= 4
 
-precision = 7
+precision = 9
 if str(lb)[0:precision] == str(ub)[0:precision]:
     approx = float(str(lb)[0:precision])
-    print("π = %.05f" %approx)
+    print("π = %.07f" %approx)
 else:
     print("The lower bound and the upper bound do not match for this precision.")
+
+print("Calculated Pi with n=%d and precision=%d in %f seconds" %(n, precision, time.time() - st))

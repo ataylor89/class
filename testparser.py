0b84327f3dabf15b9377ec224886a087e15175e2
Andrew Taylor <rolyata47@gmail.com>
2025-07-30 16:21:33 -0500

    Making -e a required option

--- testparser.py
+++ testparser.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(
         description='We are testing out the argparse library',
         epilog='This text should appear at the bottom')
 
-parser.add_argument('-e', '--exponent')
+parser.add_argument('-e', '--exponent', required=True)
 parser.add_argument('-s', '--save', action='store_true')
 
 args = parser.parse_args()
